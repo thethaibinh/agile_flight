da1cf3e0a789e44f9e07abd752254fdd83c79148
Binh Nguyen <thethaibinh@gmail.com>
2024-09-13 17:17:08 +1000

    Fixed not saving a result for skipped trials

--- envtest/ros/benchmarking_node.py
+++ envtest/ros/benchmarking_node.py
@@ -224,8 +224,7 @@ class Evaluator:
         self.is_skipped = True
         print("Skipping this trial due to simulation quality issues")
         with open("summary.yaml", "w") as f:
-            tmp = ''
-            yaml.safe_dump(tmp, f)
+            f.write('')
         rospy.signal_shutdown("Skipped this trial")
 
     def printSummary(self):
