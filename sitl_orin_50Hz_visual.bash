81573268037d3e5ce2000713c487c6356888d2b2
Thai Binh Nguyen <thethaibinh@gmail.com>
2024-11-16 12:50:10 +1100

    Fixed SITL connection

--- sitl_orin_50Hz_visual.bash
+++ sitl_orin_50Hz_visual.bash
@@ -3,7 +3,7 @@
 # Launch the simulator, unless it is already running
 if [ -z $(pgrep main) ]
 then
-  roslaunch dna sitl.launch fcu_url:=udp://0.0.0.0:5000@5000 &
+  roslaunch dna sitl.launch fcu_url:=udp://0.0.0.0:14550@14555 &
   ROS_PID="$!"
   echo $ROS_PID
   sleep 1
